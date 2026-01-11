74b0d8ea4eebc9bbffb03b78f16388de40e46a18
efmomoh <efmomoh25@gmail.com>
2026-01-11 23:28:32 +0800

    Uses the camel case variables

--- week01/Exercise1/Program.cs
+++ week01/Exercise1/Program.cs
@@ -8,9 +8,9 @@ class Program
 
         //ask user for their name 
         Console.Write("What is your first name? ");
-        string firstname = Console.ReadLine();
+        string firstName = Console.ReadLine();
         Console.Write("What is your last name? ");
-        string lastname = Console.ReadLine();
-        Console.Write($"Your name is {lastname}, {firstname} {lastname}.");
+        string lastName = Console.ReadLine();
+        Console.Write($"Your name is {lastName}, {firstName} {lastName}.");
     }
 }
