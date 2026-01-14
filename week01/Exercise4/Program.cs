58bc7681e1db6910df7d41f709a98b278177c4cf
efmomoh <efmomoh25@gmail.com>
2026-01-14 22:55:03 +0800

    Add list of numbers and compute the total

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -5,5 +5,32 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise4 Project.");
+
+        // create a list variable
+        List<int> num = new List<int>();
+
+        int userInput = -1;
+
+        while (userInput != 0)
+        {
+            Console.Write("Enter a list of numbers (type 0 when finished): ");
+            userInput = int.Parse(Console.ReadLine());
+
+            if (userInput != 0)
+            {
+                num.Add(userInput);
+
+            }
+        }
+
+        // add the total using foreah loop 
+        int total = 0;
+        foreach (int number in num)
+        {
+            total += number;
+        }
+
+        Console.WriteLine($"Total is : {total}");
+
     }
 }
