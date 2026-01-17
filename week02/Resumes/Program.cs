b0742ec9d944240ecb542045af1db29008aba2e3
efmomoh <efmomoh25@gmail.com>
2026-01-17 08:45:36 +0800

    Adds new Program dot C-Sharp file

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Resumes Project.");
+
+        // job instance initialization of class constructors
+        Job job1 = new Job("Microsoft", "Software Developer", 2023, 2028);
+        Job job2 = new Job("Apple", "Manager", 2025, 2023);
+
+        // job list and resume instance initialization of class constructor
+        //  and add jobs to resume list
+        List<Job> myJobs = new List<Job>();
+        Resumes myResumes = new Resumes("Enssah Fayia Momoh", myJobs);
+        myResumes.__jobs.Add(job1);
+        myResumes.__jobs.Add(job2);
+
+        // call the methods or behavior
+        myResumes.Display();
     }
 }
