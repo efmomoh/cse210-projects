b7b101e34dad81e732ab02e4e796c7c17c29f6a7
efmomoh <efmomoh25@gmail.com>
2026-01-23 10:34:01 +0800

    Update the program and the errors

--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -30,7 +30,58 @@ class Program
             total += number;
         }
 
+        // find the average 
+        float average = (float)total / num.Count;
+
+        // find the maximum or highest number 
+        int maximum_num = num[0];
+
+        foreach (int numb in num)
+        {
+            if (numb > maximum_num)
+            {
+                maximum_num = numb;
+            }
+        }
+
+
+        // find the minimun number 
+        int min_num = num[0];
+        foreach (int numb in num)
+        {
+            if (numb < min_num)
+            {
+                min_num = numb;
+            }
+        }
+
+        // determining the smallest positive number 
+        int smallestPositiveNumber = int.MaxValue;
+        foreach (int numb in num)
+        {
+            if (numb > 0 && numb < smallestPositiveNumber)
+            {
+                smallestPositiveNumber = numb;
+            }
+        }
+
+        // let's sort the numbers in the list 
+        num.Sort();
+
+        // and then let's finally print our output to the console 
+
+
         Console.WriteLine($"Total is : {total}");
+        Console.WriteLine($"The Average is: {average}");
+        Console.WriteLine($"The largest number is: {maximum_num}");
+        Console.WriteLine($"The smallest positive number is: {smallestPositiveNumber}");
+        Console.WriteLine($"The sorted list is:");
+
+        // let's do a foreach loop to print out the sorted numbers 
+        foreach(int numb in num)
+        {
+            Console.WriteLine(numb);
+        }
 
     }
 }
