83948e9f3ac2d6472cfcd4859bbe5ddb92719a49
efmomoh <efmomoh25@gmail.com>
2026-01-30 21:08:37 +0800

    Modifies main program content

--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -4,6 +4,39 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the YouTubeVideos Project.");
+        Console.WriteLine("Hello World! This is the YouTubeVideos Project.\n");
+
+        // let's instantiate the video object
+        // video one
+        Video video1 = new Video("Leraning C#", "EFM", 300);
+        video1.AddComment(new Comment("Enssah", "Great explanation!"));
+        video1.AddComment(new Comment("Fayia", "Very helpful."));
+        video1.AddComment(new Comment("Momoh", "I finally understand abstraction."));
+
+        // video two 
+        Video video2 = new Video("OOP Basics", "TechWorld", 420);
+        video2.AddComment(new Comment("Princess", "Clear and simple.")); 
+        video2.AddComment(new Comment("Chemist Momoh", "Love the examples."));
+        video2.AddComment(new Comment("Reading", "Please make more videos."));
+        
+        // video three
+        Video video3 = new Video("Why Learning Programming?", "CodeHub", 180);
+        video3.AddComment(new Comment("Taurus", "Motivating content."));
+        video3.AddComment(new Comment("Don", "This inspired me."));
+        video3.AddComment(new Comment("Jesus Matters Most", "Well said."));
+
+        // create a list and store videos in a list 
+        List<Video> videos = new List<Video>
+        {
+            video1, video2, video3
+        };
+
+        // let's use a foreach loop to loop over each video and display the videos
+        foreach(Video video in videos)
+        {
+            video.DisplayVideoInformation();
+        }
+
+       
     }
 }
