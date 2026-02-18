cc08e8a5e604c7c3484784772e1bf2f7a5985000
efmomoh <efmomoh25@gmail.com>
2026-02-19 02:07:25 +0800

    Adds main program class

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the EternalQuest Project.");
+        Console.WriteLine("\nHello World! This is the EternalQuest Project.");
+
+        // instantiate the goal manager
+        GoalManager manager = new GoalManager();
+        manager.Start();
     }
 }
