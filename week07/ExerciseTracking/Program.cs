a03d7ac2e9f0f7d2ef97ed95710c2d6f44e89d12
efmomoh <efmomoh25@gmail.com>
2026-02-20 04:08:55 +0800

    Modifies main program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+        Console.WriteLine("\nHello World! This is the ExerciseTracking Project.");
+
+        // create list of activities (Polymorphism)
+        List<Activity> activities = new List<Activity>();
+
+        // let's instantiate objects
+        activities.Add(new RunningActivity("19 Feb 2026", 30, "km", 4.8));
+        activities.Add(new CyclingActivity("20 Feb 2026", 45, "km", 20.0));
+        activities.Add(new SwimmingActivity("21 Feb 2026", 40, "km", 20));
+
+        // display summaries
+        foreach (Activity activity in activities)
+        {
+            DisplayActivityDetails(activity);
+
+        }
+
+        Console.WriteLine();
+
+    }
+    static void DisplayActivityDetails(Activity a)
+    {
+        Console.WriteLine(a.GetSummary());
     }
 }
